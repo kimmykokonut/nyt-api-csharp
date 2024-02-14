6c45c06a3a34f641baf2dc1bef66d653b3ea61c9
Kim Robinson <kimmykokonut@gmail.com>
2024-02-13 20:57:59 -0800

    turn api response from json str to json obj to c# obj

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 using System.Threading.Tasks;
 using RestSharp;
 using ConsoleApiCall.Keys;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
 namespace ApiTest;
 
@@ -11,7 +14,22 @@ class Program
   {
     Task<string> apiCallTask = ApiHelper.ApiCall(EnvironmentVariables.ApiKey);
     string result = apiCallTask.Result;
-    Console.WriteLine(result);
+    // Console.WriteLine(result);
+    JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+    // Console.WriteLine(jsonResponse["results"]);
+    List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+    
+    foreach (Article article in articleList)
+    {
+      Console.WriteLine($"Section: {article.Section}");
+      Console.WriteLine($"Title: {article.Title}");
+      Console.WriteLine($"Abstract: {article.Abstract}");
+      Console.WriteLine($"Url: {article.Url}");
+      Console.WriteLine($"Byline: {article.Byline}");
+      Console.WriteLine($"Item_Type: {article.Item_Type}");
+      Console.WriteLine("-----------------------------");
+
+    }
   }
 }
 class ApiHelper
